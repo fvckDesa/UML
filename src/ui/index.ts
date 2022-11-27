483d8b0279fd3452651f5debb258a86a91fd60c1
Luca De Santis <lucaroma2705@gmail.com>
2022-11-27 22:19:45 +0100

    Export PanelElement

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,3 +3,4 @@ export { default as SelectField } from "./SelectField";
 export { default as CheckboxField } from "./CheckboxField";
 export { default as VariableField } from "./VariableField";
 export { default as LoaderButton } from "./LoaderButton";
+export { default as PanelElement } from "./PanelElement";
