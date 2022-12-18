fe01f397228bea78fb54ac69e6a5b34467981924
Luca De Santis <lucaroma2705@gmail.com>
2022-12-18 17:40:29 +0100

    Put options in element

--- src/ui/SelectField.tsx
+++ src/ui/SelectField.tsx
@@ -1,34 +1,46 @@
-import { forwardRef, ReactElement, Ref, SelectHTMLAttributes } from "react";
-
-type ProbablyArr<T> = T | T[];
+import { forwardRef, ForwardedRef, SelectHTMLAttributes } from "react";
+import { ListItem, isListGroup } from "@src/types/optionsList";
+import { getLabel, getValue } from "@src/utils/optionsList";
 
 interface IProps extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string;
-  children?: ProbablyArr<ReactElement<HTMLOptionElement | HTMLOptGroupElement>>;
+  options: ListItem[];
 }
 
-const SelectField = forwardRef(
-  (
-    { label, children, name, ...selectProps }: IProps,
-    ref: Ref<HTMLSelectElement>
-  ) => {
-    return (
-      <div>
-        <label className="block w-full mb-1" htmlFor={name}>
-          {label}
-        </label>
-        <select
-          ref={ref}
-          name={name}
-          id={name}
-          className="w-full px-3 py-1.5 border-2 border-gray-300 rounded-lg transition-all outline-none focus:border-blue-600"
-          {...selectProps}
-        >
-          {children}
-        </select>
-      </div>
-    );
-  }
-);
+function SelectField(
+  { label, name, options, ...selectProps }: IProps,
+  ref: ForwardedRef<HTMLSelectElement>
+) {
+  return (
+    <div>
+      <label className="block w-full mb-1" htmlFor={name}>
+        {label}
+      </label>
+      <select
+        ref={ref}
+        name={name}
+        id={name}
+        className={`w-full px-3 py-1.5 border-2 border-gray-300 rounded-lg transition-all outline-none focus:border-blue-600`}
+        {...selectProps}
+      >
+        {options?.map((el) =>
+          isListGroup(el) ? (
+            <optgroup key={el.label} label={el.label}>
+              {el.items?.map((option) => (
+                <option key={getLabel(option)} value={getValue(option)}>
+                  {getLabel(option)}
+                </option>
+              ))}
+            </optgroup>
+          ) : (
+            <option key={getLabel(el)} value={getValue(el)}>
+              {getLabel(el)}
+            </option>
+          )
+        )}
+      </select>
+    </div>
+  );
+}
 
-export default SelectField;
+export default forwardRef(SelectField);
