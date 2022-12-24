9609e80d18ce49eccf2685d6b0b75553d98c96a9
Luca De Santis <lucaroma2705@gmail.com>
2022-12-24 23:51:55 +0100

    Extract parameters fields in a reusable component

--- src/components/MethodModal.tsx
+++ src/components/MethodModal.tsx
@@ -3,78 +3,36 @@ import ReactDOM from "react-dom";
 import type { Method } from "@src/types/class";
 import type { Modal } from "@src/types/modal";
 // components
-import {
-  InputField,
-  SelectField,
-  CheckboxField,
-  LoaderButton,
-  VariableField,
-  ModalForm,
-} from "@src/ui";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { InputField, SelectField, CheckboxField, ModalForm } from "@src/ui";
 // hooks
-import { useForm, useFieldArray } from "react-hook-form";
-// icon
-import { faXmark, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 import { validation } from "@src/utils/validate";
-import { JAVA_TYPES } from "@src/data/types";
 import TypeList from "./TypeList";
+import ParametersField from "@src/ui/ParametersField";
 
 type IProps = Modal<Method>;
 
 function ClassModal({ data, onSave, onClose }: IProps) {
-  const listRef = useRef<HTMLUListElement>(null);
-  const [isTop, setIsTop] = useState(true);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
     watch,
     control,
-    setFocus,
     setValue,
   } = useForm<Method>();
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "parameters",
-  });
 
   useEffect(() => {
     if (!data) return;
     for (const [key, value] of Object.entries(data)) {
-      if (key === "parameters") {
-        value.forEach(append);
-        continue;
-      }
       setValue(key as keyof Method, value);
     }
-
-    return () => {
-      remove(data.parameters.map((_, i) => i));
-    };
   }, [data]);
-
-  useLayoutEffect(() => {
-    listRef.current?.scrollTo({
-      top: listRef.current?.scrollHeight ?? 0,
-      behavior: "smooth",
-    });
-    setFocus(`parameters.${fields.length - 1}.name`);
-  }, [fields.length]);
-
-  function handlerAppend() {
-    append({ name: "", type: "", isArray: false });
-  }
 
   function onSubmit(newData: Method) {
     onSave(newData);
     onClose();
-  }
-
-  function handlerParametersScroll() {
-    if (!listRef.current) return;
-    setIsTop(listRef.current.scrollTop === 0);
   }
 
   return ReactDOM.createPortal(
@@ -89,38 +47,7 @@ function ClassModal({ data, onSave, onClose }: IProps) {
         options={["public", "private", "protected", "package"]}
         {...register("visibility", { value: "public" })}
       />
-      <div className="overflow-x-hidden">
-        <header className="flex justify-between items-center p-1">
-          <h2>Parameters</h2>
-          <button
-            className="btnAction w-6 h-6"
-            type="button"
-            onClick={handlerAppend}
-            data-testid="add-parameter"
-          >
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </header>
-        <ul
-          ref={listRef}
-          className={`flex flex-col items-center gap-2 max-h-[120px] p-2 !pr-[50px] border-t-2 ${
-            !isTop ? "border-blue-500" : "border-transparent"
-          } mr-[-50px] overflow-y-scroll scroll-smooth`}
-          onScroll={handlerParametersScroll}
-        >
-          {fields.map((field, i) => (
-            <VariableField
-              key={field.id}
-              isArray={watch(`parameters.${i}.isArray`)}
-              //! resolve type error
-              register={register as any}
-              index={i}
-              errors={errors.parameters?.[i]}
-              onRemove={remove}
-            />
-          ))}
-        </ul>
-      </div>
+      <ParametersField control={control as any} register={register as any} />
       <InputField
         label="Name"
         error={errors.name?.message}
---
+++ src/ui/ParametersField.tsx
@@ -0,0 +1,121 @@
+// types
+import type { Variable } from "@src/types/class";
+import type { Control, FieldValues, UseFormRegister } from "react-hook-form";
+import type { UIEvent } from "react";
+// components
+import ScrollContainer from "./ScrollContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// hooks
+import { useFieldArray, useFormState } from "react-hook-form";
+import { useState } from "react";
+// icons
+import {
+  faLayerGroup,
+  faPlus,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+
+interface FieldValuesWithParameters extends FieldValues {
+  parameters: Variable[];
+}
+
+interface IProps {
+  control: Control<FieldValuesWithParameters>;
+  register: UseFormRegister<FieldValuesWithParameters>;
+}
+
+function ParametersField({ control, register }: IProps) {
+  const [isTop, setIsTop] = useState(true);
+  const { errors } = useFormState({ control });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "parameters",
+  });
+
+  function handlerAppend() {
+    append({ name: "", type: "", isArray: false });
+  }
+
+  function handlerScroll(e: UIEvent) {
+    if (e.target instanceof HTMLElement) {
+      setIsTop(e.target.scrollTop === 0);
+    }
+  }
+
+  return (
+    <div className="overflow-x-hidden select-none">
+      <header className="flex justify-between items-center p-1">
+        <h2>Parameters</h2>
+        <button
+          className="btnAction w-6 h-6"
+          type="button"
+          data-testid="add-parameter"
+          onClick={handlerAppend}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </header>
+      <ScrollContainer
+        className={`border-t-2 ${
+          !isTop ? "border-blue-500" : "border-transparent"
+        }`}
+        maxHeight={120}
+        onScroll={handlerScroll}
+      >
+        <ul className="flex flex-col items-center gap-2 p-2">
+          {fields.map((field, i) => (
+            <li
+              key={field.id}
+              className="w-full flex justify-between items-center"
+            >
+              <p>{i + 1}.</p>
+              <input
+                className={`w-[30%] px-1 text-sm leading-6 outline-none border-2 ${
+                  errors.parameters?.[i]?.name
+                    ? "!border-red-500"
+                    : "border-gray-300"
+                } rounded focus:border-blue-500`}
+                type="text"
+                placeholder="Name"
+                autoComplete="off"
+                data-testid="parameter-name"
+                {...register(`parameters.${i}.name`, { required: true })}
+              />
+              <input
+                className={`w-[30%] px-1 text-sm leading-6 outline-none border-2 ${
+                  errors.parameters?.[i]?.type
+                    ? "!border-red-500"
+                    : "border-gray-300"
+                } rounded focus:border-blue-500`}
+                type="text"
+                placeholder="Type"
+                autoComplete="off"
+                data-testid="parameter-type"
+                {...register(`parameters.${i}.type`, { required: true })}
+              />
+              <label className="relative w-6 h-6 rounded overflow-hidden">
+                <input
+                  type="checkbox"
+                  className="hidden peer"
+                  {...register(`parameters.${i}.isArray`)}
+                />
+                <div className="w-full h-full flex justify-center items-center cursor-pointer peer-checked:bg-blue-500 peer-checked:text-white">
+                  <FontAwesomeIcon icon={faLayerGroup} />
+                </div>
+              </label>
+              <button
+                className="btnAction w-6 h-6"
+                type="button"
+                onClick={() => remove(i)}
+              >
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </ScrollContainer>
+    </div>
+  );
+}
+
+export default ParametersField;
